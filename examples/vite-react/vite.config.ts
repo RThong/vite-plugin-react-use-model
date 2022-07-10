bdd5893ce47fb2803b5d6759c76b2943033297fe
RThong <403216075@qq.com>
2022-07-10 23:59:25 +0800

    chore: remove tsup

--- examples/vite-react/vite.config.ts
+++ examples/vite-react/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import template from 'vite-plugin-react-use-model';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), template()],
+  plugins: [
+    react(),
+    template({
+      modelDir: path.resolve(process.cwd(), 'src/models'),
+    }),
+  ],
 });
